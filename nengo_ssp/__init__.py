356ad57debac6e486406288e82f15c9ba2fe0864
nsdumont <nicole.s.dumont@gmail.com>
2020-08-31 00:50:23 -0400

    Code

---
+++ nengo_ssp/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for Spatial Semantic Pointers."""
+
+__author__ = """Nicole Sandra-Yaffa Dumont"""
+__email__ = 'nicole.s.dumont@gmail.com'
+__version__ = '0.1.0'
+
+import nengo_ssp.dists
+import nengo_ssp.hrr_algebra
+import nengo_ssp.plotting
+import nengo_ssp.spatial_semantic_pointer
+import nengo_ssp.utils
+import nengo_ssp.vector_generation
